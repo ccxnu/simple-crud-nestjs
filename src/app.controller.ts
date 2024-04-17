8fa6f048083405c14d6fb19c338af56cfbc999e5
Pablo Cuenca <108037606+cuencapablo@users.noreply.github.com>
2024-04-17 17:33:51 -0500

    Create crud for users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // Constructor
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Routing
   getHello(): string {
     return this.appService.getHello();
   }
